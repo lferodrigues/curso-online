b10356e57a1d9c2c9628cb7aa2413350ee0991a9
Felipe Rodrigues <65414548+lferodrigues@users.noreply.github.com>
2024-12-28 11:21:16 -0300

    Update funcao-video.js

--- Assets/js/funcao-video.js
+++ Assets/js/funcao-video.js
@@ -1,25 +1,3 @@
-// Atualiza o ano no rodapé
-document.getElementById('currentYear').textContent = new Date().getFullYear();
-
-// Desabilitar o botão direito do mouse
-document.addEventListener("contextmenu", (event) => {
-    event.preventDefault();
-    alert("Função desabilitada!");
-});
-
-// Bloquear teclas relacionadas a ferramentas de desenvolvedor
-document.addEventListener("keydown", (event) => {
-    if (
-        event.key === "F12" || 
-        (event.ctrlKey && event.shiftKey && event.key === "I") || 
-        (event.ctrlKey && event.shiftKey && event.key === "J") || 
-        (event.ctrlKey && event.key === "U") || 
-        (event.ctrlKey && event.key === "S") // Bloquear Ctrl+S
-    ) {
-        event.preventDefault();
-        alert("Ação bloqueada!");
-    }
-});
 
 // Função para carregar um vídeo no iframe
 function changeVideo(videoSrc) {
